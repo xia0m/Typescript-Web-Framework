8b91e1cd653d1f925c1a39b9c2a70b4d4986ddeb
xia0m <alexma325@gmail.com>
2019-09-09 11:43:59 -0700

    feat: added initial user class defination

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world!');
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+user.set({ name: 'newName' });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
